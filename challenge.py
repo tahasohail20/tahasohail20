ea83dc414c45ac8af93fe3509cb8a47eba016ff7
tahasohail20 <60719670+tahasohail20@users.noreply.github.com>
2022-06-12 19:16:50 -0400

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,14 @@
+name = input("Enter your Name: ")
+age = int(input("How old are you?:"))
+
+if 18 <= age < 31:
+    print("Welcome to the club 18-30 holidays, {0}".format(name))
+else:
+    print("You cannot got on this trip")
+
+
+
+
+
+
+
